508950c4bb7acf5654ab730a258af2950de577bc
James Milam <jmilam343@gmail.com>
2015-09-16 10:26:35 -0400

    added a template file for the quiz friday

---
+++ notes/robot_geometry/home_work/quiz_1.py
@@ -0,0 +1,31 @@
+# This file will be used during the first quiz for robot geometry
+
+import numpy as np
+import math as m
+import rg_tools as rgt
+
+# Useful functions to know for quizz
+# np.cross(a, b) = cross product
+#   - requires a and b to be row vectors
+# np.linalg.norm(a) = magnitude
+# a.transpose() = transpose of vector/matrix a
+
+# Template for a vector
+FP_1 = np.matrix([[0], [0], [0]])
+
+# Template of the rotation matrix
+R_1_2 = np.matrix([[0, 0, 0],
+                   [0, 0, 0],
+                   [0, 0, 0]])
+
+# Template of the transformation matrix
+T_1_2 = np.matrix([[0, 0, 0, 0],
+                   [0, 0, 0, 0],
+                   [0, 0, 0, 0],
+                   [0, 0, 0, 1]])
+
+# Problem 1
+print("\nProblem 1\n")
+
+# Problem 2
+print("\nProblem 2\n")
