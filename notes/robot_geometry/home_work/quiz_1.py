e4da862dc6d86249d34c9254363090d6df7ba6b6
jbm950 <jmilam343@gmail.com>
2015-09-19 10:13:27 -0400

    quiz_1.py in robot geometry now actually reflects work done on quiz 1

--- notes/robot_geometry/home_work/quiz_1.py
+++ notes/robot_geometry/home_work/quiz_1.py
@@ -27,5 +27,30 @@ T_1_2 = np.matrix([[0, 0, 0, 0],
 # Problem 1
 print("\nProblem 1\n")
 
+print("\nPart a\n")
+
+AP_Borig = np.matrix([[2], [5], [1]])
+thetaz = 50
+thetax = 90
+
+R_A_1 = rgt.rotation_about_z(thetaz)
+R_1_B = rgt.rotation_about_x(thetax)
+
+print("\n", R_A_1, "\n\n", R_1_B, "\n")
+
+R_A_B = R_A_1 * R_1_B
+
+print(R_A_B, "\n\n")
+
+T_A_B = rgt.trans_matrix_form(R_A_B, AP_Borig)
+
+print(T_A_B)
+
+print("\nPart b\n")
+
+[theta, mvec] = rgt.rev_gen_vec_rotation_matrix(R_A_B)
+
+print("\n", mvec, "\n", theta)
+
 # Problem 2
 print("\nProblem 2\n")
