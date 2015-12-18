7d2cd372b13c6585350a9b14e5a9efc1dc803558
jbm950 <jmilam343@gmail.com>
2015-12-17 22:52:57 -0500

    took the 3rd exam today and the code reflects the exam

--- notes/robot_geometry/home_work/quiz_3.py
+++ notes/robot_geometry/home_work/quiz_3.py
@@ -1,12 +1,24 @@
-import numpy as np
 import rg_tools as rgt
 
-s = [8, 4, 6]
-theta = 60
+m1 = [8, 4, 6]
+the1 = 60
 
-r = rgt.Quaternion([0, 1, 2, 3])
-q = rgt.Quaternion.compose(s, theta)
-qinv = q.inv()
+r = rgt.Quaternion([0, 1, 0, 0])
+q1 = rgt.Quaternion.compose(m1, the1)
+q1inv = q1.inv()
 
-rprime = q * r * qinv
-print(rprime.numbers)
+m2 = (q1*r*q1inv).numbers[1:4]
+the2 = 76
+
+q2 = rgt.Quaternion.compose(m2, the2)
+q2inv = q2.inv()
+
+m3 = [0, 1, 0]
+the3 = 50
+
+q3 = rgt.Quaternion.compose(m3, the3)
+q3inv = q3.inv()
+
+q4 = q1inv * q2inv * q3inv
+print(q4.numbers)
+print(q4.decompose())
